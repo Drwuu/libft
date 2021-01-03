e58644ed3a207ebd3eebef2c519939c8883ba067
Drwuu <wu.ludwig@gmail.com>
2021-01-03 15:27:38 +0100

    lstmap resolved

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -3,41 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lwourms <lwourms@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: drwuu <drwuu@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/23 00:51:53 by drwuu             #+#    #+#             */
-/*   Updated: 2021/01/03 14:08:34 by lwourms          ###   ########lyon.fr   */
+/*   Updated: 2021/01/03 15:27:00 by drwuu            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list      *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-    t_list  *new_list;
-    t_list  *first_elem;
-    
-    if (!lst || !f || !del)
-        return (NULL);
-    if (!(new_list = ft_lstnew(f(lst->content))))
-        return (NULL);
-    first_elem = new_list;
-    lst = lst->next))
-        return (first_elem);
-    while (lst)
-    {
-        if (!(new_list = ft_lstnew(f(lst->content))))
-        {
-            ft_lstclear(&first_elem, del);
-            return (NULL);
-        }
-        if (lst->next)
-        {
-            lst = lst->next;
-            new_list->next = lst;
-        }
-        else
-            new_list->next = NULL;
-    }
-    return (first_elem);
+	t_list	*new_list;
+	t_list	*first_elem;
+	
+	if (!lst || !f || !del)
+		return (NULL);
+	if (!(new_list = ft_lstnew(f(lst->content))))
+		return (NULL);
+	first_elem = new_list;
+	lst = lst->next;
+	while (lst)
+	{
+		if (!(new_list->next = ft_lstnew(f(lst->content))))
+		{
+			ft_lstclear(&first_elem, del);
+			return (NULL);
+		}
+		lst = lst->next;
+		new_list = new_list->next;
+	}
+	new_list->next = NULL;
+	return (first_elem);
 }
