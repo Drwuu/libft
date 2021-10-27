d1d1041f2234222b83db1e5ecdfbf2c77bfa3e4f
lwourms <lwourms@student.42lyon.fr>
2021-10-27 10:10:44 +0200

    - Rework of ft_ischar

--- srcs/compare/ft_ischar.c
+++ srcs/compare/ft_ischar.c
@@ -6,43 +6,19 @@
 /*   By: lwourms <lwourms@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 14:33:27 by lwourms           #+#    #+#             */
-/*   Updated: 2021/05/16 19:08:04 by lwourms          ###   ########.fr       */
+/*   Updated: 2021/10/27 09:53:15 by lwourms          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/libft.h"
+#include "libft.h"
 
-static int	process(const char *s, const char *c)
+/*
+ * Check if str contains the char c
+ */
+int	ft_ischar(const char *str, char c)
 {
-	int	ret;
-	int	i;
-	int	j;
-
-	i = 0;
-	j = 0;
-	ret = 0;
-	while (s[i])
-	{
-		while (c[j])
-		{
-			if (s[i] == c[j++])
-			{
-				ret = 1;
-				j = 0;
-				break ;
-			}
-		}
-		if (!ret)
-			return (0);
-		ret = 0;
-		i++;
-	}
-	return (1);
-}
-
-int	ft_ischar(const char *s, const char *c)
-{
-	if (!s)
-		return (-1);
-	return (process(s, c));
+	while (*str)
+		if (*str++ == c)
+			return (1);
+	return (0);
 }
